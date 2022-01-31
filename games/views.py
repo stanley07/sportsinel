9840046064cba86935e6d1541a04f55bac2cb166
stanley07 <ezebo001@gmail.com>
2022-01-31 15:24:43 +0100

    upgrade commit

--- games/views.py
+++ games/views.py
@@ -91,21 +91,22 @@ def away_loose(request):
 def raw_predictions(request):
     df = pd.read_csv("media/csv/predictions_with_gridsearch.csv")    
     df1 = df[['match_datetime', 'country', 'league', 'home_team', 'away_team', 'predicted_home_score', 'predicted_away_score']]
+    
     #conditions = [df1['predicted_home_score'] > df1['predicted_away_score'],
      #             df1['predicted_home_score'] < df1['predicted_away_score']]
     #choices = ['home_team', 'away_team'] 
     #df1['prediction'] = np.select(conditions, choices, default='Tie')
     df1['predicted'] = np.where((df1['predicted_home_score'] <= df1['predicted_away_score']), df1['away_team'], np.nan)
     df1 = df1.dropna()
-             
-    ##df1 = df1.set_axis(['Match_Datetime', 'Country', 'League', 'Home_team', 'Away_team','Predicted_home_score', 'Predicted_away_score'], axis=1)
+    df4 = df1.to_html()
+
    # df1['total_predicted_goals'] = df1['predicted_home_score'] + df1['predicted_away_score']
     #df1 = df.rename(columns={'match_datetime': 'Match_Datetime', 'country': 'Country', 'league': 'League', 'home_team': 'Home', 'away_team': 'Away', 'predicted_home_score': 'predicted_home_score', 'predicted_away_score': 'predicted_away_score', 'home_team': 'win' })
    ## df2 = df1.reset_index()   
     #df1 = df1.sort_values(by=["win"], ascending=False)
-    df2 = df1
-    raw = df2.to_html()
-  
+    
+    raw = df4
+
     return render(request, 'raw.html', {
         'raw': raw
         })
