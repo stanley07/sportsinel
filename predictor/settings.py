ddd5a265478740bf9d3785994a8e0d321f9a1c0e
stanley07 <ezebo001@gmail.com>
2022-01-31 20:36:46 +0100

    upgrade commit

--- predictor/settings.py
+++ predictor/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rpjvopi9_6t1dt4w4ynf*^o38o@wxqwm^#o^p)^nns8o9&a3di'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
