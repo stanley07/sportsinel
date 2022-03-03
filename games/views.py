78254c789b2dada94db539954f59b8acf2998995
stanley07 <ezebo001@gmail.com>
2022-03-03 04:59:47 +0100

    base commit

--- games/views.py
+++ games/views.py
@@ -18,9 +18,9 @@ def privacy(request):
 def home_wins(request):
     df = pd.read_csv("media/csv/predictions_with_gridsearch.csv")    
     df1 = df[['match_datetime', 'country', 'league', 'home_team', 'away_team', 'predicted_home_score', 'predicted_away_score', 'home_team']]
-    df1 = df1.set_axis(['Date', 'Country', 'League', 'Home', 'Away','predicted_homescore', 'predicted_awayscore', 'Prediction'], axis=1)
-    df1 = df1.round({'predicted_home_score' : 1, 'predicted_away_score':1})
-    df1 = df1.sort_values(by=["predicted_homescore"], ascending=False)
+    df1 = df1.set_axis(['Date', 'Country', 'League', 'Home', 'Away','predicted_home_score', 'predicted_away_score', 'Prediction'], axis=1)
+    #df1 = df1.round({'predicted_home_score' : 1, 'predicted_away_score':1})
+    df1 = df1.sort_values(by=["predicted_home_score"], ascending=False)
     
 
     df2 = df1.head(30)
@@ -35,7 +35,7 @@ def home_wins(request):
 
 def home_loose(request):
     df = pd.read_csv("media/csv/predictions_with_gridsearch.csv")     
-    df1 = df[['match_datetime', 'country', 'league', 'home_team', 'away_team', 'predicted_home_score', 'predicted_away_score', 'away_team']]
+    df1 = df[['match_datetime', 'country', 'league', 'home_team', 'away_team', 'predicted_home_score', 'predicted_away_score', 'home_team']]
     df1 = df1.set_axis(['Date', 'Country', 'League', 'Home', 'Away','Predicted_home_score', 'Predicted_away_score', 'Prediction'], axis=1)
     #df1 = df.rename(columns={'match_datetime': 'Match_Datetime', 'country': 'Country', 'league': 'League', 'home_team': 'Home', 'away_team': 'Away', 'predicted_home_score': 'predicted_home_score', 'predicted_away_score': 'predicted_away_score', 'home_team': 'win' })
     df1 = df1.sort_values(by=["Predicted_home_score"], ascending=False)
@@ -193,7 +193,7 @@ def top_pick(request):
     
     #df1 = df1.drop(['Predicted_home_score', 'Predicted_away_score'], axis = 1)
     df2 = df2.head(10) 
-    df2 = df2.style   
+    df2 = df2.style.set_precision(2) 
     df2 = df2.to_html()
    
        
